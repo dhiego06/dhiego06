cb63a21684de0e19187d0a5b508451ec871c36ad
Dhiego Carmo <dhiegosc@gmail.com>
2022-05-08 18:29:43 -0300

    ok 2

---
+++ DataBase/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Build", "S3251:Supply an implementation for the partial method, otherwise this call will be ignored.", Justification = "<Pending>", Scope = "member", Target = "~M:DataBase.Data.CERTIFICAContext.OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder)")]
+[assembly: SuppressMessage("Build", "S3251:Supply an implementation for this partial method.", Justification = "<Pending>", Scope = "member", Target = "~M:DataBase.Data.CERTIFICAContext.OnModelCreatingPartial(Microsoft.EntityFrameworkCore.ModelBuilder)")]
+[assembly: SuppressMessage("Build", "S3251:Supply an implementation for the partial method, otherwise this call will be ignored.", Justification = "<Pending>", Scope = "member", Target = "~M:DataBase.LICITANTEContext.OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder)")]
+[assembly: SuppressMessage("Build", "S3251:Supply an implementation for this partial method.", Justification = "<Pending>", Scope = "member", Target = "~M:DataBase.LICITANTEContext.OnModelCreatingPartial(Microsoft.EntityFrameworkCore.ModelBuilder)")]
+[assembly: SuppressMessage("Build", "S101:Rename class 'CERTIFICAContext' to match pascal case naming rules, consider using 'CertificaContext'.", Justification = "<Pending>", Scope = "type", Target = "~T:DataBase.Data.CERTIFICAContext")]
+[assembly: SuppressMessage("Build", "S101:Rename class 'LICITANTEContext' to match pascal case naming rules, consider using 'LicitanteContext'.", Justification = "<Pending>", Scope = "type", Target = "~T:DataBase.LICITANTEContext")]
